bddf5f8273decd53ea92c7b40e18bff4c4ec5274
mansobeach <mansobeach@gmail.com>
2025-11-05 08:50:42 +0100

    fabfile has been added

---
+++ fabfile.py
@@ -0,0 +1,68 @@
+import glob
+import os
+
+from fabric import task, executor
+from loguru import logger
+
+
+@task
+def build(context):
+    '''
+    Build bcops binary wheel distribution
+    '''
+    cmd = 'python setup.py bdist_wheel'
+    logger.info(cmd)
+    context.run(cmd)
+
+
+@task
+def install(context):
+    '''
+    Install bcops latest built package with pip
+    '''
+    list_of_files   = glob.glob('dist/bcops*')
+    latest_package  = max(list_of_files, key=os.path.getctime)
+    cmd             = f"pip install {latest_package}"
+    logger.info(cmd)
+    context.run(cmd)
+
+
+@task
+def uninstall(context):
+    '''
+    Uninstall bcops package with pip
+    '''
+    cmd = 'pip uninstall -y bcops'
+    logger.info(cmd)
+    context.run(cmd)
+
+
+@task
+def update(context):
+    '''
+    Update bcops python package:
+    > build 
+    > uninstall
+    > install
+    '''
+    build(context)
+    uninstall(context)
+    install(context)
+
+
+@task
+def test(context):
+    '''
+    Execution of unit tests:
+    '''
+    test_unit_bcops_maap_query_datatake(context)
+    
+    
+@task
+def test_unit_bcops_maap_query_datatake(context):
+    '''
+    Execution of unit test main_test_unit_bcops_tools.py 
+    '''
+    cmd = 'pytest -rpf -s test/test_unit_bcops_maap_query_datatake.py'
+    logger.info(cmd)
+    context.run(cmd)
