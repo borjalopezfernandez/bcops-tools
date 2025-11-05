21ac6e44d1b9db06272fa8dcac60c00deb89a054
mansobeach <mansobeach@gmail.com>
2025-11-05 08:30:29 +0100

    unit test for level-0 S1_RAW__0S S2_RAW__0S S3_RAW__0S

---
+++ setup.py
@@ -0,0 +1,20 @@
+"""
+pip install -e src/
+python3 src/test.py
+"""
+from setuptools import setup, Extension, find_packages
+
+setup(
+    license_files           =   ['LICENSE'],
+    python_requires         =   ">3.11",       # only applies to sdist
+    name                    =   "bcops",
+    version                 =   "0.0.1",
+    description             =   "BCOPS tools",
+    package_dir             =   {"": "src"},
+    scripts                 =   ['src/bcops_maap_query_datatake'],
+    install_requires        =   ['pytest', 'loguru', 'click', 'pystac_client'],
+    extras_require          =   {},
+)
+
+print("Completed execution of python setup pyeocfi:")
+print("\U0001F606")
---
+++ test/test_unit_bcops_maap_query_datatake.py
@@ -0,0 +1,73 @@
+import sys
+import os
+import time
+import pytest
+from importlib.metadata import version
+
+from loguru import logger
+
+@pytest.fixture(scope = "function", autouse = True)
+def test_setup():
+    logger.info('test')
+    logger.info('----------------- test_setup() -------------------')
+
+
+@pytest.fixture
+def print_separator():
+    logger.debug("\n=========================================================================")
+
+
+def test_version(print_separator):
+    """
+    GIVEN test_conversion
+    WHEN 
+    THEN
+    """
+    logger.info(f"START : {sys._getframe().f_code.co_name} / {version('bcops')}")
+    cmd             = f'bcops_maap_query_datatake --version'
+    logger.info(cmd)
+    exit = os.system(cmd)
+    assert(exit == 0)
+    logger.info(f"END : {sys._getframe().f_code.co_name} / {version('bcops')}")
+
+
+def test_l0_S1_RAW__0S(print_separator):
+    """
+    GIVEN test_conversion
+    WHEN 
+    THEN
+    """
+    logger.info(f"START : {sys._getframe().f_code.co_name} / {version('bcops')}")
+    cmd             = f'bcops_maap_query_datatake --type S1_RAW__0S --start 2025-07-22T00:00:00Z --end 2025-07-22T02:00:00Z'
+    logger.info(cmd)
+    exit = os.system(cmd)
+    assert(exit == 0)
+    logger.info(f"END : {sys._getframe().f_code.co_name} / {version('bcops')}")
+
+
+def test_l0_S2_RAW__0S(print_separator):
+    """
+    GIVEN test_conversion
+    WHEN 
+    THEN
+    """
+    logger.info(f"START : {sys._getframe().f_code.co_name} / {version('bcops')}")
+    cmd             = f'bcops_maap_query_datatake --type S2_RAW__0S --start 2025-10-22T00:00:00Z --end 2025-10-22T02:00:00Z'
+    logger.info(cmd)
+    exit = os.system(cmd)
+    assert(exit == 0)
+    logger.info(f"END : {sys._getframe().f_code.co_name} / {version('bcops')}")
+
+
+def test_l0_S3_RAW__0S(print_separator):
+    """
+    GIVEN test_conversion
+    WHEN 
+    THEN
+    """
+    logger.info(f"START : {sys._getframe().f_code.co_name} / {version('bcops')}")
+    cmd             = f'bcops_maap_query_datatake --type S3_RAW__0S --start 2025-11-04T00:00:00Z --end 2025-11-04T02:00:00Z'
+    logger.info(cmd)
+    exit = os.system(cmd)
+    assert(exit == 0)
+    logger.info(f"END : {sys._getframe().f_code.co_name} / {version('bcops')}")
