2e17ef4c11cc7c5e40c71dec81acbe7e44bd8b2c
Borja Lopez Fernandez <mansobeach@gmail.com>
2025-12-15 06:37:06 +0100

    0.0.9 test of bcops_maap_query_datatake to cover TOM-MC1-SW2

--- test/test_unit_bcops_maap_query_datatake.py
+++ test/test_unit_bcops_maap_query_datatake.py
@@ -44,7 +44,7 @@ def test_l0_S1_RAW__0S(print_separator):
     THEN
     """
     logger.info(f"START : {sys._getframe().f_code.co_name} / {version('bcops')}")
-    cmd             = f'bcops_maap_query_datatake --type S1_RAW__0S --start 2025-07-22T00:00:00Z --end 2025-07-22T02:00:00Z'
+    cmd             = f'bcops_maap_query_datatake --type S1_RAW__0S --start 2025-11-21T00:29:45Z --end 2025-12-12T00:29:58Z'
     logger.info(cmd)
     exit = os.system(cmd)
     assert(exit == 0)
@@ -62,10 +62,10 @@ def test_csv_l0_S1_RAW__0S(print_separator):
     import os
     import glob
 
-    for f in glob.glob('bcops_maap_query_datatake_S1_RAW__0S_20250722T000000_20250722T020000*'):
+    for f in glob.glob('bcops_maap_query_datatake_S1_RAW__0S_20251121T002945_20251212T002958*'):
         logger.debug(f'Removing previous result file: {f}')
         os.remove(f)
-    cmd             = f'bcops_maap_query_datatake --type S1_RAW__0S --start 2025-07-22T00:00:00Z --end 2025-07-22T02:00:00Z -C'
+    cmd             = f'bcops_maap_query_datatake --type S1_RAW__0S --start 2025-11-21T00:29:45Z --end 2025-12-12T00:29:58Z -C'
     logger.info(cmd)
     exit = os.system(cmd)
     assert(exit == 0)
@@ -92,7 +92,7 @@ def test_l1a_S1_SCS__1S(print_separator):
     THEN
     """
     logger.info(f"START : {sys._getframe().f_code.co_name} / {version('bcops')}")
-    cmd             = f'bcops_maap_query_datatake --type S1_SCS__1S --start 2025-07-22T00:00:00Z --end 2025-07-22T02:00:00Z'
+    cmd             = f'bcops_maap_query_datatake --type S1_SCS__1S --start 2025-11-21T00:29:45Z --end 2025-12-12T00:29:58Z'
     logger.info(cmd)
     exit = os.system(cmd)
     assert(exit == 0)
@@ -113,12 +113,12 @@ def test_csv_l1a_S1_SCS__1S(print_separator):
     for f in glob.glob('bcops_maap_query_datatake_S1_SCS__1S_20250722T000000_20250722T020000*'):
         logger.debug(f'Removing previous result file: {f}')
         os.remove(f)
-    cmd             = f'bcops_maap_query_datatake --type S1_SCS__1S --start 2025-07-22T00:00:00Z --end 2025-07-22T02:00:00Z -C'
+    cmd             = f'bcops_maap_query_datatake --type S1_SCS__1S --start 2025-11-21T00:29:45Z --end 2025-12-12T00:29:58Z -C'
     logger.info(cmd)
     exit = os.system(cmd)
     assert(exit == 0)
 
-    results = glob.glob('bcops_maap_query_datatake_S1_SCS__1S_20250722T000000_20250722T020000*')
+    results = glob.glob('bcops_maap_query_datatake_S1_SCS__1S_20251121T002945_20251212T002958*')
     assert(len(results) == 1)
     logger.debug(f'Found result file: {results[0]}')
 
@@ -139,7 +139,7 @@ def test_l1b_S1_DGM__1S(print_separator):
     THEN
     """
     logger.info(f"START : {sys._getframe().f_code.co_name} / {version('bcops')}")
-    cmd             = f'bcops_maap_query_datatake --type S1_DGM__1S --start 2025-07-22T00:00:00Z --end 2025-07-22T02:00:00Z'
+    cmd             = f'bcops_maap_query_datatake --type S1_DGM__1S --start 2025-11-21T00:29:45Z --end 2025-12-12T00:29:58Z'
     logger.info(cmd)
     exit = os.system(cmd)
     assert(exit == 0)
@@ -157,15 +157,15 @@ def test_csv_l1b_S1_DGM__1S(print_separator):
     import os
     import glob
 
-    for f in glob.glob('bcops_maap_query_datatake_S1_DGM__1S_20250722T000000_20250722T020000*'):
+    for f in glob.glob('bcops_maap_query_datatake_S1_DGM__1S_20251121T002945_20251212T002958*'):
         logger.debug(f'Removing previous result file: {f}')
         os.remove(f)
-    cmd             = f'bcops_maap_query_datatake --type S1_DGM__1S --start 2025-07-22T00:00:00Z --end 2025-07-22T02:00:00Z -C'
+    cmd             = f'bcops_maap_query_datatake --type S1_DGM__1S --start 2025-11-21T00:29:45Z --end 2025-12-12T00:29:58Z -C'
     logger.info(cmd)
     exit = os.system(cmd)
     assert(exit == 0)
 
-    results = glob.glob('bcops_maap_query_datatake_S1_DGM__1S_20250722T000000_20250722T020000*')
+    results = glob.glob('bcops_maap_query_datatake_S1_DGM__1S_20251121T002945_20251212T002958*')
     assert(len(results) == 1)
     logger.debug(f'Found result file: {results[0]}')
 
@@ -179,7 +179,6 @@ def test_csv_l1b_S1_DGM__1S(print_separator):
     logger.info(f"END : {sys._getframe().f_code.co_name} / {version('bcops')}")
 
 
-
 def test_l0_S2_RAW__0S(print_separator):
     """
     GIVEN test_conversion
@@ -187,35 +186,35 @@ def test_l0_S2_RAW__0S(print_separator):
     THEN
     """
     logger.info(f"START : {sys._getframe().f_code.co_name} / {version('bcops')}")
-    cmd             = f'bcops_maap_query_datatake --type S2_RAW__0S --start 2025-10-22T00:00:00Z --end 2025-10-22T02:00:00Z'
+    cmd             = f'bcops_maap_query_datatake --type S2_RAW__0S --start 2025-12-12T00:29:58Z --end 2026-01-02T00:30:12Z -D'
     logger.info(cmd)
     exit = os.system(cmd)
     assert(exit == 0)
     logger.info(f"END : {sys._getframe().f_code.co_name} / {version('bcops')}")
 
 
-def test_l1a_S2_SCS__0S(print_separator):
+def test_l1a_S2_SCS__1S(print_separator):
     """
     GIVEN test_conversion
     WHEN 
     THEN
     """
     logger.info(f"START : {sys._getframe().f_code.co_name} / {version('bcops')}")
-    cmd             = f'bcops_maap_query_datatake --type S2_SCS__1S --start 2025-10-22T00:00:00Z --end 2025-10-22T02:00:00Z'
+    cmd             = f'bcops_maap_query_datatake --type S2_SCS__1S --start 2025-12-12T00:29:58Z --end 2026-01-02T00:30:12Z -D'
     logger.info(cmd)
     exit = os.system(cmd)
     assert(exit == 0)
     logger.info(f"END : {sys._getframe().f_code.co_name} / {version('bcops')}")
 
 
-def test_l1b_S2_DGM__0S(print_separator):
+def test_l1b_S2_DGM__1S(print_separator):
     """
     GIVEN test_conversion
     WHEN 
     THEN
     """
     logger.info(f"START : {sys._getframe().f_code.co_name} / {version('bcops')}")
-    cmd             = f'bcops_maap_query_datatake --type S2_DGM__1S --start 2025-10-22T00:00:00Z --end 2025-10-22T02:00:00Z'
+    cmd             = f'bcops_maap_query_datatake --type S2_DGM__1S --start 2025-12-12T00:29:58Z --end 2026-01-02T00:30:12Z -D'
     logger.info(cmd)
     exit = os.system(cmd)
     assert(exit == 0)
