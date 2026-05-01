bf031f21100e38d0deb6039145df1c0356079c66
Borja Lopez Fernandez <mansobeach@gmail.com>
2026-05-01 12:48:15 +0200

    maap errors are caught gracefully

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     license_files           =   ['LICENSE'],
     python_requires         =   '>=3.11',       # only applies to sdist
     name                    =   'bcops',
-    version                 =   '0.0.23',
+    version                 =   '0.0.24',
     description             =   "BCOPS tools",
     package_dir             =   {"bcops": "src/bcops"},
     package_data            =   {'bcops': ['data/*']},
